dbf11c3e2b4ad51b0fb25a7845a7721882598f41
Baver Kaçar <baverkacar@gmail.com>
2021-11-10 14:28:27 +0300

    Database settedup

--- profiles_api/settings.py
+++ profiles_api/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+AUTH_USER_MODEL = 'p_api.UserProfile'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
